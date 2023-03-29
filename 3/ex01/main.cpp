37828d85c71b7b4c025bfb1f02379dbbef770911
Hugo COULON <hucoulon@e3r4p5.clusters.42paris.fr>
2023-03-29 18:13:30 +0200

    fin day03 debut day04

--- 3/ex01/main.cpp
+++ 3/ex01/main.cpp
@@ -5,5 +5,8 @@ int	main(void)
 {
 	ScavTrap hugo("hugo");
 	ScavTrap pierre(hugo);
+	hugo.attack("pierre");
+	pierre.guardGate();
+	pierre.takeDamage(50);
 	return (0);
 }
---
+++ 3/ex02/FragTrap.cpp
@@ -0,0 +1,43 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+	this->hit_point = 100;
+	this->energy_point = 100;
+	this->attack_damage = 30;
+	std::cout << this->name << " open his eyes for the first time, and see the world" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& copy) : ClapTrap(copy)
+{
+	std::cout << this->name << " acknoledge the fact that he is a copy" << std::endl;
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << this->name << " know is time is near it's end, he close his eyes and die happily" << std::endl;
+}
+
+/* ------------------- Fonction --------------------- */
+
+void		FragTrap::attack(const std::string& target)
+{
+	if (this->hit_point == 0)
+	{
+		std::cout << this->name << " can't attack, HE IS DEAD" << std::endl;
+	}
+	else if (this->energy_point == 0)
+	{
+		std::cout << this->name << " can't attack, he is tired" << std::endl;
+	}
+	else
+	{
+		std::cout << this->name << " hit " << target << " with a huge hit, causing " << this->attack_damage << " points of damage, Damn what a hit" << std::endl;
+		this->energy_point--;
+	}
+}
+
+void		FragTrap::highFivesGuys(void)
+{
+	std::cout << "Hey guys, Let's High Fives !!!! It's so coooolll !!!! " << std::endl;
+}
